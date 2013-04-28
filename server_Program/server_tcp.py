0d4072e5dafa6f23e69316a4f2d043b85e1436c2
tmkasun <tmkasun@gmail.com>
2013-04-28 16:15:06 +0530

    add imei number aproval

--- server_Program/server_tcp.py
+++ server_Program/server_tcp.py
@@ -32,6 +32,7 @@ import threading
 import socket
 import MySQLdb
 import time
+from datetime import datetime
 
 
 
@@ -55,6 +56,7 @@ class newConnection(threading.Thread):
     channel , address = detailsPair
     self.channel = channel
     self.address = address
+    self.approvedImei = False
     self.splitedGpsData = ''
     self.connectToDB()
     threading.Thread.__init__(self)
@@ -75,13 +77,45 @@ class newConnection(threading.Thread):
     #===========================================================================
 #    self.cursor.execute(""" insert into vehicleStatus(imei,latestConnectionCreatedDate,currentOnlineStatus) values({imei},UTC_TIMESTAMP(),1) on duplicate key update latestConnectionCreatedDate = UTC_TIMESTAMP(),currentOnlineStatus = 1  """).format()
     
+    #===========================================================================
+    # to be implimented log incompatible IMEI patterns request while checking IMEI patter befor continuing
+    # allow viewing server connection log via web page 
+    #===========================================================================
+    
+    recivedDataFromGpsDevice = self.channel.recv(2048) #2048 is the buffer size
+    self.splitedGpsData = recivedDataFromGpsDevice.split(',') #split string by ','
+  
+
+    #===========================================================================
+    # check weather imei number is approved or not if it is not in approved_imei numbsers list connection will be closed
+    #===========================================================================
+    
+    self.cursor.execute("select * from approved_imei")
+    approvedImeiList = self.cursor.fetchall()
+    print approvedImeiList
+    for tuple in approvedImeiList:
+      if self.splitedGpsData[16][5:] == tuple[0]:
+        self.approvedImei = True
+        break
+    
+    if not self.approvedImei:
+      print "Not an approved IMEI number, Waiting for new connection...."
+      sql = """ insert IGNORE into not_approved_imei values("{}",{},"{}") """.format(self.splitedGpsData[16][5:],0,datetime.now())
+      self.cursor.execute(sql)
+      self.channel.close()
+      return 0
+      
+    #===========================================================================
+    # create function for store GPS data
+    #===========================================================================
+      
     while True:
       #=========================================================================
       # need to debug firmware info and alarm - move, speed, batteries, help me! or "" after F or L Signal quality    F 
       #=========================================================================
       recivedDataFromGpsDevice = self.channel.recv(2048) #2048 is the buffer size
       self.splitedGpsData = recivedDataFromGpsDevice.split(',') #split string by ','
-    
+
       #===============================================================================
       # Decode sat longitude and latitude
       #===============================================================================
@@ -155,7 +189,7 @@ def main():
         break
     
     except socket.error:
-        print "Port already opend auto reconnect in 5s......"
+        print "Delaying connection for 5 seconds due to previously failed connection attempt..."
         time.sleep(5)
         continue
     
@@ -179,7 +213,7 @@ if __name__ == "__main__":
   time.sleep(0.1)
   print '|      | |      | |       | |        \     |  |   |                   '
   time.sleep(0.1)
-  print '|         \____/| |          \____/   \___/\  |_  |_              v0.2'
+  print '|         \____/| |          \____/   \___/\  |_  |_              v0.3'
   time.sleep(0.1)
   print '|______         | |_______                                            '
   time.sleep(0.1)
@@ -189,7 +223,7 @@ if __name__ == "__main__":
   time.sleep(0.1)
   print ' ______/   _____/   \_____/                                           '
   time.sleep(0.1)
-  print 'Contact SysCall for any bug reports at syscall@groups.facebook.com    '
+  print 'Contact us for any bug reports at syscall@knnect.com    '
   time.sleep(0.1)
   print '\n'
 
