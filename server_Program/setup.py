0275dcc810d3d97ee7e58193b9ebddd665d4b81e
tmkasun <tmkasun@gmail.com>
2013-05-04 08:16:20 +0530

    add initial code for setup.py and last commit before the midSemester exams :)

---
+++ server_Program/setup.py
@@ -0,0 +1,8 @@
+#! /user/bin/python
+
+""" 
+Configure operating syster befor running server_tcp.py
+check system dependancies and install if not available 
+"""
+
+print "Setting up server_tcp.py is not implimentd yet"
