5781feb6a2d12759569aea27399bd23d349c7542
Kasun Thennakoon <tmkasun@gmail.com>
2014-04-10 01:07:57 +0530

    define maps ajax request actions,seperate user login logic

--- codei/application/controllers/login.php
+++ codei/application/controllers/login.php
@@ -2,14 +2,25 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
+if (!isset($_SESSION)) {
+	session_start();
+}
+
 class Login extends CI_Controller {
 
 	public function index() {
-		$this -> load -> helper(array('form'));
-		$this -> load -> view('login/index');
-		// $this -> load -> model('user');
-		// $result = $this -> user -> login("123456", "123456");
-		// echo $result -> loginApprovalBit;
+		if (is_user_logged_in()) {
+			redirect('maps', 'refresh');
+		} else {
+			$this -> load -> helper(array('form'));
+			$this -> load -> view('login/index');
+		}
+	}
+
+	public function logout() {
+		$this -> session -> unset_userdata('logged_in');
+		session_destroy();
+		redirect(base_url(), 'refresh');
 	}
 
 }
--- codei/application/controllers/maps.php
+++ codei/application/controllers/maps.php
@@ -5,9 +5,8 @@ session_start();
 class Maps extends CI_Controller {
 
 	public function index() {
-		if ($this -> session -> userdata('logged_in')) {
-			$session_data = $this -> session -> userdata('logged_in');
-
+		if (is_user_logged_in()) {
+			$session_data = is_user_logged_in();
 			$content_data = array('computer_number' => $session_data['computer_number'], 'full_name' => $session_data['full_name']);
 			$layout_data = array('title' => "Welcome to maps", 'content' => "maps/home", 'content_data' => $content_data);
 			$this -> load -> view('layouts/inner_layout', $layout_data);
@@ -19,10 +18,14 @@ class Maps extends CI_Controller {
 		}
 	}
 
-	public function logout() {
-		$this -> session -> unset_userdata('logged_in');
-		session_destroy();
-		redirect('login', 'refresh');
+	public function get_coordinates() {
+		if ($this -> input -> is_ajax_request()) {
+			$this -> load -> model('coordinate', "code");
+			$query = $this -> code -> get_live_status();
+			$this -> output -> set_content_type('application/json') -> set_output(json_encode($query->result()));
+		} else {
+			echo "This method is not allowed";
+		}
 	}
 
 }
