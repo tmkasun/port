38504b8cb9d66445fdb36ff7eaaaa4340b401b34
Kasun Thennakoon <tmkasun@gmail.com>
2014-04-10 01:06:39 +0530

    update user login and logout actions

--- codei/application/models/user.php
+++ codei/application/models/user.php
@@ -1,4 +1,7 @@
 <?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
 class User extends CI_Model {
 	public function login($computer_number, $password) {
 		$this -> db -> select('loginApprovalBit');
@@ -27,38 +30,10 @@ class User extends CI_Model {
 		}
 	}
 
-	public function update_login($computer_number)
-	{
-		$data = array(
-		'computer_number' => $computer_number,
-		'date_time' => date("Y-m-d H:i:s"),
-		'ip_addr' => $this->input->ip_address(),
-		'host_name' => gethostbyaddr($_SERVER['REMOTE_ADDR']),
-		'browser_details' => $this->input->user_agent()
-		);
-		
-		$this->db->insert('track',$data);
-	}
-	
-	function get_real_ip_address() {
+	public function update_login($computer_number) {
+		$data = array('computer_number' => $computer_number, 'date_time' => date("Y-m-d H:i:s"), 'ip_addr' => $this -> input -> ip_address(), 'host_name' => gethostbyaddr($_SERVER['REMOTE_ADDR']), 'browser_details' => $this -> input -> user_agent());
 
-		if (isset($_SERVER)) {
-			if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
-				return $_SERVER["HTTP_X_FORWARDED_FOR"];
-			if (isset($_SERVER["HTTP_CLIENT_IP"]))
-				return $_SERVER["HTTP_CLIENT_IP"];
-			return $_SERVER["REMOTE_ADDR"];
-		}
-
-		if (getenv(“HTTP_X_FORWARDED_FOR))
-			return getenv(“HTTP_X_FORWARDED_FOR);
-		if (getenv(“HTTP_CLIENT_IP))
-			return getenv(“HTTP_CLIENT_IP);
-		if (getenv(“REMOTE_ADDR))
-			return getenv(“REMOTE_ADDR);
-
-		return  “UNKNOWN”;
-
+		$this -> db -> insert('track', $data);
 	}
 
 }
